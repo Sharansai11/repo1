499b0e107c91ec2389124e250c663960f31973fb
Sharansai11 <sharansaitelukula@gmail.com>
2024-06-19 14:24:12 +0530

    new code   navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,11 +2,7 @@ import React from 'react'
 
 function navbar() {
     return (
-        <div>new textdfv
-            f
-            gb ocde
-            code
-            <h1> fshdvfbv</h1>
+        <div> new code 
         </div>
     )
 }
